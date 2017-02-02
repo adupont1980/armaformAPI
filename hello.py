add166bd8fd9f20251301116cf2fd5a1d06e79a6
Anthony <anthony.dupont@eduwell.cz>
2017-02-02 14:58:13 +0100

    day2

--- hello.py
+++ hello.py
@@ -39,8 +39,8 @@ if not MONGO_URL:
 
 app.config['MONGO_URI'] = MONGO_URL
 # mongo = PyMongo(app)
-mongo = MongoClient(MONGO_URL)
-db = client.test
+client = MongoClient(MONGO_URL)
+mongo = client.test
 # connection = pymongo.MongoClient("ds135029.mlab.com", 35029)
 # db = connection["heroku_p754dw74"]
 # db.authenticate("russianBallet", "Axonian456")
