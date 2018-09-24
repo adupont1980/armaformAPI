72b633686c1078fb3944becef872e586187bde04
Anthony <anthony.dupont@eduwell.cz>
2018-09-24 13:03:53 +0200

    remove duration from mail

--- hello.py
+++ hello.py
@@ -727,22 +727,11 @@ def send_email():
     ]
     mailInfo = mailCollection.aggregate(pipeline)
     mailConfig  = mailInfo.next()
-
     
     masterConfig = mailConfig['master']
 
     me = masterConfig[0]['email']
     password = masterConfig[0]['mail_pwd']
-    # print(mailInfo['subject'])
-    # print(mailInfo[0]['email'])
-
-    # if appName == 'ballet':
-    # app.config['MAIL_SERVER']='smtp.live.com'
-    # app.config['MAIL_PORT'] = 25
-    # app.config['MAIL_USERNAME'] = 'armanaly@hotmail.com'
-    # app.config['MAIL_PASSWORD'] = 'Goodbye2012'
-    # app.config['MAIL_USE_TLS'] = True
-    # app.config['MAIL_USE_SSL'] = False
 
     mail.init_app(app)
 
@@ -790,7 +779,7 @@ def send_email():
         bckMessage = MIMEText(prenom + " " + nom + " has registred to the "+ course + 
                 " course for "+ stage +
                 "\n\n age:  " + formData['age'] +
-                "\n duration: "+ formData['duration'] +
+                
                 "\n Residence: "+ formData['residence'] +
                 "\n Years of  experience: "+ formData['years_of_experience'] +
                 "\n email: "+ email )
